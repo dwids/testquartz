60d0eb0f4022b09c73e9ac245223cebf4c1f95c0
dwids <sidwell.david@gmail.com>
2026-04-27 10:10:01 +1000

    add deploy workflow and set baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dwids.github.io/testquartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
